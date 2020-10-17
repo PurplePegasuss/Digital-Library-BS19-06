3be011597c010f29834e119de94ad1a37a0eb7da
Dmitry Chermnykh <27727194+Chermnyx@users.noreply.github.com>
2020-10-17 14:01:40 +0500

    Fix flask secret key type

--- digital_library/cfg.py
+++ digital_library/cfg.py
@@ -1,3 +1,3 @@
 import os
 DATABASE = os.environ.get('DATABASE', 'sqlite:///./data/db.sqlite')
-SECRET_KEY = os.environ.get('SECRET_KEY', 'очень сложный ключ')
+SECRET_KEY = bytes(os.environ.get('SECRET_KEY', 'очень сложный ключ'))
