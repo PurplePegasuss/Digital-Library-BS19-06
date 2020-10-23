d41f35667c0c8fd2d2bf6cb57f81867b4510ca51
Roman <nabroman42@icloud.com>
2020-10-23 21:21:24 +0300

    Displaying list of the materials is fixed

--- digital_library/views/index.py
+++ digital_library/views/index.py
@@ -9,17 +9,44 @@ def index():
     return render_template('index.html')
 
 
+# Author: Roman
 @index_router.route('/list_of_materials')
 def list_of_materials():
     # TODO: check whether the database is not empty
-    data = [(material.Title, material.id) for material in MATERIAL.select()]
+    # TODO: check whether attributes are not None and handle it
+
+    data = []
+    for material in MATERIAL.select():
+        material_attributes = {}
+        material_attributes['id'] = material.id
+        material_attributes['type'] = material.Type
+        material_attributes['tags'] = material.tags
+        material_attributes['title'] = material.Title
+        material_attributes['authors'] = material.authors
+        material_attributes['description'] = material.Description
+
+        data.append(material_attributes)
+
     return render_template('list_of_materials.html', data = data)
 
 
-@index_router.route('/list_of_materials/<materialId>')
-def material(materialId = None):
-    # TODO: if no such material exist then also 404
-    if materialId is None:
+# Author: Roman
+@index_router.route('/list_of_materials/<material_id>')
+def material(material_id = None):
+    if material_id is None:
         return render_template('404.html'), 404
 
-    return render_template('comment_section.html')
+    # TODO: if no such material exist then also 404
+    # Retrieve the material from DB
+    material = MATERIAL.get(id = material_id)
+
+    # Get its attributes
+    data = {}
+    data['id'] = material.id
+    data['type'] = material.Type
+    data['tags'] = material.tags
+    data['title'] = material.Title
+    data['authors'] = material.authors
+    data['description'] = material.Description
+
+    return render_template('comment_section.html', data = data)
