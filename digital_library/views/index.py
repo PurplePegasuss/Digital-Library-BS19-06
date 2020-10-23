c1964b89fff06da37af3652a4676c5f942861193
Dmitry Chermnykh <27727194+Chermnyx@users.noreply.github.com>
2020-10-23 14:28:13 +0500

    Add front page list query

--- digital_library/views/index.py
+++ digital_library/views/index.py
@@ -1,8 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app, request, abort
+
+from digital_library.db import MATERIAL
 
 index_router = Blueprint('index', __name__, template_folder='templates')
 
 
 @index_router.route('/')
 def index():
-    return render_template('index.html', hello='Hello World')
+    try:
+        page = int(request.args.get('page', 0))
+        if page < 0:
+            abort(400, description="The page can't be less than 0")
+            return
+    except ValueError:
+        abort(404, description='Page is not found')
+        return
+
+    materials = (MATERIAL
+                 .select()
+                 .join(MATERIAL.tags.get_through_model())
+                 .switch(MATERIAL)
+                 .join(MATERIAL.authors.get_through_model())
+                 .paginate(page, current_app.config['MATERIALS_PER_PAGE'])
+                 )
+
+    return render_template('index.html', materials=materials, hello='world')
