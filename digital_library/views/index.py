2289e6e2b1be904d33dcaddd6376ef96b49e625f
Roman <nabroman42@icloud.com>
2020-10-23 19:45:43 +0300

    Comment section branch is modified

--- digital_library/views/index.py
+++ digital_library/views/index.py
@@ -1,27 +1,27 @@
-from flask import Blueprint, render_template, current_app, request, abort
-
-from digital_library.db import MATERIAL
+from flask import Blueprint, render_template
+from digital_library.db import *
 
 index_router = Blueprint('index', __name__, template_folder='templates')
 
 
 @index_router.route('/')
 def index():
-    try:
-        page = int(request.args.get('page', 0))
-        if page < 0:
-            abort(400, description="The page can't be less than 0")
-            return
-    except ValueError:
-        abort(404, description='Page is not found')
-        return
+    return render_template('index.html')
 
-    materials = (MATERIAL
-                 .select()
-                 .join(MATERIAL.tags.get_through_model())
-                 .switch(MATERIAL)
-                 .join(MATERIAL.authors.get_through_model())
-                 .paginate(page, current_app.config['MATERIALS_PER_PAGE'])
-                 )
 
-    return render_template('index.html', materials=materials, page=page)
+@index_router.route('/list_of_materials')
+def list_of_materials():
+    # TODO: check whether the database is not empty
+    data = [(material.Title, material.id) for material in MATERIAL.select()]
+    return render_template('list_of_materials.html', data = data)
+
+
+@index_router.route('/list_of_materials/<materialId>')
+def material(materialId = None):
+    # TODO: if no such material exist then also 404
+    if materialId is None:
+        return render_template('404.html'), 404
+
+    comment_text = COMMENT.get(COMMENT.commented_material)
+
+    return render_template('comment_section.html')
