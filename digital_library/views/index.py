fc72f6d64d25b70f16a95169057911836d338ee6
Dmitry Chermnykh <27727194+Chermnyx@users.noreply.github.com>
2020-10-24 18:03:03 +0500

    Restore original idnex page

--- digital_library/views/index.py
+++ digital_library/views/index.py
@@ -1,12 +1,29 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, current_app, request, abort
 from digital_library.db import *
 
-index_router = Blueprint('index', __name__, template_folder = 'templates')
+index_router = Blueprint('index', __name__, template_folder='templates')
 
 
 @index_router.route('/')
 def index():
-    return render_template('index.html')
+    try:
+        page = int(request.args.get('page', 0))
+        if page < 0:
+            abort(400, description="The page can't be less than 0")
+            return
+    except ValueError:
+        abort(404, description='Page is not found')
+        return
+
+    materials = (MATERIAL
+                 .select()
+                 .join(MATERIAL.tags.get_through_model())
+                 .switch(MATERIAL)
+                 .join(MATERIAL.authors.get_through_model())
+                 .paginate(page, current_app.config['MATERIALS_PER_PAGE'])
+                 )
+
+    return render_template('index.html', materials=materials, page=page)
 
 
 @index_router.errorhandler(404)
