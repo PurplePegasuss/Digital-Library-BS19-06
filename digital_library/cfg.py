a2df6a735dd755d47f387d02fbe6cbf54da294ca
Roman <nabroman42@icloud.com>
2020-10-27 19:54:20 +0300

    CONFIG materials_per_page is 4 now;
    for the simplicity of testing

--- digital_library/cfg.py
+++ digital_library/cfg.py
@@ -3,5 +3,5 @@ import os
 DATABASE = os.environ.get('DATABASE', 'sqlite:///./data/db.sqlite')
 SECRET_KEY = os.environ.get('SECRET_KEY', 'очень сложный ключ').encode('utf-8')
 
-MATERIALS_PER_PAGE = int(os.environ.get('MATERIALS_PER_PAGE', 20))
+MATERIALS_PER_PAGE = int(os.environ.get('MATERIALS_PER_PAGE', 4))  # TODO: is 4 for the moment, make it 20 after!
 COMMENTS_PER_PAGE = int(os.environ.get('COMMENTS_PER_PAGE', 3))     # TODO: is 3 for the moment, make it 20 after!
