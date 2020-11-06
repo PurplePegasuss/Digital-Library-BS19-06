d327919e8a915fdf0dff9d7431eb714578cbf0aa
Feyd-Rauth <atreides.pasha@yandex.ru>
2020-11-06 11:19:47 +0300

    separated backend API from auth.py to views/auth.py

--- digital_library/auth.py
+++ digital_library/auth.py
@@ -9,12 +9,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Email, Length
 
-from flask import request, render_template, redirect, url_for, Blueprint, abort
 
 login_manager = LoginManager()
 login_manager.init_app(app=app)
-
-auth_router = Blueprint('auth', __name__, template_folder='templates')
 
 
 class LoginForm(FlaskForm):
@@ -59,37 +56,6 @@ def _hash_password(password: str) -> str:
     return str(sha512(password.encode("utf-8")).hexdigest())
 
 
-@auth_router.route('/login', methods=['GET', 'POST'])
-def validate_login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        email = form.email.data
-        password = form.password.data
-        try:
-            login(email, password)
-            return redirect(url_for('index.index'))
-        except AuthException as e:
-            abort(400, description=str(e))
-
-    return render_template('login.html', form=form)
-
-
-@auth_router.route('/register', methods=['GET', 'POST'])
-def validate_register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        first_name = form.first_name.data
-        second_name = form.second_name.data
-        email = form.email.data
-        password = form.password.data
-        try:
-            register(email, password, first_name, second_name)
-        except AuthException as e:
-            abort(400, description=str(e))
-
-    return render_template('register.html', form=form)
-
-
 def login(email: str, password: str) -> USER:
     password_hash = _hash_password(password)
     user = USER.get_or_none((USER.Email == email) & (USER.PasswordHash == password_hash))
@@ -100,13 +66,6 @@ def login(email: str, password: str) -> USER:
     login_user(user)
 
     return user
-
-
-@auth_router.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index.index'))
 
 
 def register(email: str, password: str, first_name: str = "anon", second_name: str = "anon") -> USER:
--- digital_library/views/__init__.py
+++ digital_library/views/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 from digital_library.views.index import index_router
-from digital_library.auth import auth_router
+from digital_library.views.auth import auth_router
 
 
 def create_views(app: Flask):
